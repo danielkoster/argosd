c9120b2eb719ffc7e7eb38820086e8e197540485
Daniel Koster <daniel@khlan.nl>
2016-08-23 20:57:17 +0200

    Added setup file
    Issue #8

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+config = {
+    'name': 'ArgosD',
+    'description': 'Daemon for Argos, a Python project to keep track of series on IPTorrents.',
+    'author': 'Daniel Koster',
+    'url': 'http://gitlab.intarweb.nl/pi-projects/argosd',
+    'version': '0.1.0',
+}
+
+setup(**config)
