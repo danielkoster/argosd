2b1a891b7de3534b9f16959667306ad360185210
Daniel Koster <daniel@khlan.nl>
2016-08-23 21:20:55 +0200

    Tweaked setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 config = {
-    'name': 'ArgosD',
+    'name': 'argosd',
     'description': 'Daemon for Argos, a Python project to keep track of series on IPTorrents.',
     'author': 'Daniel Koster',
     'url': 'http://gitlab.intarweb.nl/pi-projects/argosd',
