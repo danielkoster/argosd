95d54344741463e6b99e7b61c07fa2afb7afd4f7
Daniel Koster <daniel@khlan.nl>
2016-08-24 17:26:50 +0200

    Converted double quotes to single quotes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from argosd import argosd
 import logging
 
-formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s")
+formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
 
-logfile_handler = logging.FileHandler("/var/log/argosd/argosd.log", "w")
+logfile_handler = logging.FileHandler('/var/log/argosd/argosd.log', 'w')
 logfile_handler.setLevel(logging.DEBUG)
 logfile_handler.setFormatter(formatter)
 
@@ -11,7 +11,7 @@ logger = logging.getLogger('argosd')
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logfile_handler)
 
-logger.info("ArgosD starting")
+logger.info('ArgosD starting')
 
 argosd = argosd.ArgosD()
 argosd.run()
