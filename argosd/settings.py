9e8913d2a27f552bde2eacd7eeec90b985bdf6fb
Daniel Koster <daniel@khlan.nl>
2016-08-24 17:25:32 +0200

    Added settings files

---
+++ argosd/settings.py
@@ -0,0 +1,4 @@
+try:
+    from argosd.settings_local import *
+except ImportError:
+    from argosd.settings_default import *
---
+++ argosd/settings_local_template.py
@@ -0,0 +1,8 @@
+from argosd.settings_default import *
+
+"""
+Add the URL to your IPTorrents RSS-feed.
+Make sure to check the TV-category of your liking (TV/x264 is recommended),
+and check 'Download link'.
+"""
+IPTORRENTS_RSS_FEED = ''
