63a7eecb6fcdf2a2e530675adac7349a517a34dc
Daniel Koster <daniel@khlan.nl>
2016-08-24 21:46:46 +0200

    Updated setup to reflect PEP8 standard

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ import os
 
 config = {
     'name': 'argosd',
-    'description': 'Daemon for Argos, a Python project to keep track of series on IPTorrents.',
+    'description': ('Daemon for Argos, '
+                    'a Python project to keep track of series on IPTorrents.'),
     'author': 'Daniel Koster',
     'url': 'http://gitlab.intarweb.nl/pi-projects/argosd',
     'version': '0.1.0',
