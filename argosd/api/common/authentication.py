978039236fa8e5396b1319a879616f82029d4e9a
Daniel Koster <daniel@khlan.nl>
2016-09-10 15:44:52 +0200

    Updated API auth to RESTful standard

--- argosd/api/common/authentication.py
+++ argosd/api/common/authentication.py
@@ -1,27 +1,26 @@
-import logging
-
 from functools import wraps
 from flask import request, Response
 
 from argosd import settings
 
 
-def check_authentication(secret):
-    return secret == settings.API_TOKEN
+def check_authentication(username, token):
+    return username == 'argosd' and token == settings.API_TOKEN
+
 
 def authenticate():
-    """Sends a 401 response"""
-    return Response('You have to login with proper credentials', 401)
+    """Sends a 401 Unauthorized response that enables Basic Auth"""
+    return Response('You have to login with proper credentials', 401,
+                    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
 
 def requires_authentication(function):
-    """Creates a method that can be used with @requires_authentication
+    """Creates a decorator that can be applied with @requires_authentication
     to protect an API endpoint"""
     @wraps(function)
     def decorated(*args, **kwargs):
-        secret = request.headers['X-Api-Token'] if \
-            'X-Api-Token' in request.headers else None
-
-        if not check_authentication(secret):
+        auth = request.authorization
+        if not auth or not check_authentication(auth.username, auth.password):
             return authenticate()
         return function(*args, **kwargs)
     return decorated
